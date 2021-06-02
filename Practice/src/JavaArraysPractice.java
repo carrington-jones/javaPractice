db5e379556b012e70297506a8ca39d90f31c2979
Carrington Jones <carrington.jones47@gmail.com>
2021-06-02 17:24:57 -0500

    Java arrays practice

---
+++ Practice/src/JavaArraysPractice.java
@@ -0,0 +1,22 @@
+public class JavaArraysPractice {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our zombie program!!");
+
+        String backpack[] = {"Shotgun", "Rifle", "Sniper"};
+        String zombies[] = {"Close-range zombie", "Mid-range zombie", "Long-range zombie"};
+        System.out.println("Backpack items!");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+
+        System.out.println("These are the zombies!");
+        System.out.println(zombies[0]);
+        System.out.println(zombies[1]);
+        System.out.println(zombies[2]);
+
+        int numbersZombiesHATE[] = {4, 90, 70, 123, 12, 4, 561, 1};
+        System.out.println(numbersZombiesHATE[6]);
+
+    }
+
+}
