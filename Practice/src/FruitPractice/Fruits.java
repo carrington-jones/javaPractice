8d6e2f13b1934cb3936c294cf5a72a47e8d3af36
Carrington Jones <carrington.jones47@gmail.com>
2021-06-05 10:47:20 -0500

    Pushing arrays practice

---
+++ Practice/src/FruitPractice/Fruits.java
@@ -0,0 +1,19 @@
+package FruitPractice;
+
+public class Fruits {
+    private String Apple;
+    private String Banana;
+    private String Orange;
+
+    public Fruits(String apple) {
+        Apple = apple;
+    }
+
+    public String getApple() {
+        return Apple;
+    }
+
+    public void setApple(String apple) {
+        Apple = apple;
+    }
+}
