9fcac21bd80b2deca130693311283a62b3c508b6
Carrington Jones <carrington.jones47@gmail.com>
2021-06-05 15:58:19 -0500

    Java practice

---
+++ Practice/src/com/codewithcarrington/Main.java
@@ -0,0 +1,74 @@
+package com.codewithcarrington;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+    //anything in orange are reserved keywords in Java.
+    public static void main(String[] args) {
+
+        //-----Primitive Types-----//
+//        byte hisAge = 30;
+//        byte herAge = hisAge;
+//        long viewsCount = 3123456789L; //must have L for longs
+//
+//        double price = 10.99F;//must have F for floats
+//
+//        char letter = 'A';
+//
+//        boolean isEligible = false;
+        //-----Reference Types-----//
+//        byte age = 30;
+//        Date now = new Date();
+//        System.out.println(now);
+//
+//        byte x = 1;
+//        byte y = x;
+//        x = 2;
+//        System.out.println(y); //Prints out 1 because x and y are independent.
+
+//        Point point1 = new Point(1, 1);
+//        Point point2 = point1; //Point2 does not have the same VALUE as point1 this is actually referring to the address location in the memory.
+//        point1.x = 2; //point2 is referring to new Point on line 30. So when we print point2 is prints the change made to point1.
+//        System.out.println(point2);
+
+        //-----Strings-----//
+//        String message = "  Hello World" + "!!  ";
+//
+//        System.out.println(message.startsWith("!!"));
+//        System.out.println(message.length());
+//        System.out.println(message.indexOf("o"));//Will only return the first occurrence of character. If character does not exist it will return -1
+//        System.out.println(message.replace("!","*"));//Does not modify original string. Prints new string.
+//        System.out.println(message);
+//        System.out.println(message.toLowerCase());
+//        System.out.println(message);
+//        System.out.println(message.toUpperCase());
+//        System.out.println(message);
+//        System.out.println(message.trim());//Removes white space
+
+        //-----Escape Sequences-----//
+//        String message2 = "Hello Carrington";
+//        System.out.println(message2);
+//        message2 = "Hello \"Carrington\"";
+//        System.out.println(message2);
+//        message2 = "Hello \t \"Carrington\""; // \t inserts a tab when run.
+//        System.out.println(message2);
+
+        //-----Arrays-----//
+//        int[] numbers = new int[5];
+//        numbers[0] = 1;
+//        numbers[1] = 2;
+//        System.out.println(Arrays.toString(numbers));
+//
+//        int[] numbers2 = {2, 3, 5, 1, 4};
+//        System.out.println(numbers2.length); //returns how many elements are in the array
+//        Arrays.sort(numbers2);
+//        System.out.println(Arrays.toString(numbers2));
+
+        //-----MultiDimensionalArrays-----//
+
+
+
+    }
+}
