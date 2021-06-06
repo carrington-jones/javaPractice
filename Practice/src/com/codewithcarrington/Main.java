338bbc90eed84b3e7741ba6a401a949b880fed5a
Carrington Jones <carrington.jones47@gmail.com>
2021-06-06 14:17:31 -0500

    Java practice

--- Practice/src/com/codewithcarrington/Main.java
+++ Practice/src/com/codewithcarrington/Main.java
@@ -1,8 +1,10 @@
 package com.codewithcarrington;
 
 import java.awt.*;
+import java.text.NumberFormat;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     //anything in orange are reserved keywords in Java.
@@ -68,7 +70,54 @@ public class Main {
 
         //-----MultiDimensionalArrays-----//
 
+//        int[][] numbers = new int[2][3];//First numbers sets rows and second sets columns
+//        numbers [0][0] = 1; //This changes 1st element in 1st row and 1st column
+//        numbers [1][2] = 5; //This changes 2nd row 3rd column
+//        System.out.println(Arrays.deepToString(numbers)); //In order to access the 2d array must use "deepToString" method
+
+//        int[][] numbers1 = {  {1, 2, 3}, {4, 5, 6}  }; //Another way to declare 2d array
+//        System.out.println(Arrays.deepToString(numbers1));
+
+        //-----Constants-----//
+
+//        final float PI = 3.14F; //Constants are written in all caps as seen to the left.
+//        PI = 1; //Will not work because of "final" in line above.
+
+        //-----Arithmetic Expressions-----//
+//        int result = 10 / 3; //This returns a whole number because of int. Would need to cast double to return the decimal.
+//        double result1 = (double) 10 / (double) 3;
+//        System.out.println(result1);
+
+        //-----Casting-----//
+//        short x = 1;
+//        int y = x +2;
+//        System.out.println(y);//This comes out to 3. This is called Implicit casting. short is converted to int. byte > short > int > long > float > double
+
+//        String a = "1";
+//        int b = Integer.parseInt(a) + 2;
+//        System.out.println(b);//This comes out to 3 due to the "Integer.parseInt"
+
+        //-----The Math Class-----//
+
+//        int result = Math.round(1.1F); //Rounds to 1
+//        int result1 = Math.max(1, 2); //Returns 2 because it is higher
+//        int result2 = (int) Math.round(Math.random() * 100); //Returns a random number between 1 and 100
+//        System.out.println(result2);
+
+        //-----Formatting Numbers-----//
+
+//        NumberFormat currency = NumberFormat.getCurrencyInstance();
+//        String result = currency.format(1234567.891);//Returns "$1,234,567.89"
+//        System.out.println(result);
 
 
+//        String result1 = NumberFormat.getPercentInstance().format(0.1); // This is called method chaining. This returns "10%"
+//        System.out.println(result1);
+
+        //-----Reading Input-----//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Age: ");
+//        byte age = scanner.nextByte();
+//        System.out.println("You are " + age);
     }
 }
