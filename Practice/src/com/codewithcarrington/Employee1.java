0d6e6589a79c80b89c8c8096c451ddd5676a9472
Carrington Jones <carrington.jones47@gmail.com>
2021-06-13 17:08:59 -0500

    Continued java practice

--- Practice/src/com/codewithcarrington/Employee1.java
+++ Practice/src/com/codewithcarrington/Employee1.java
@@ -4,8 +4,28 @@ public class Employee1 {
     private int baseSalary;
     private int hourlyRate;
 
-    public int calculateWage(int extraHours) {
+    public static int numberOfEmployees;
+
+    public Employee1(int baseSalary) { //Overloading this constructor allows us to implement a new employee object with only one parameter
+        this(baseSalary, 0);
+    }
+
+    public Employee1(int baseSalary, int hourlyRate) {
+        setBaseSalary(baseSalary);
+        setHourlyRate(hourlyRate);
+        numberOfEmployees++;
+    }
+
+    public static void printNumberOfEmployees(){ //Static methods can only see other static methods. For example they cannot see calculateWage method.
+        System.out.println(numberOfEmployees);
+    }
+
+    public int calculateWage(int extraHours) { //Method overloading. This method and calculate wage method on line 21 have different parameters but same method. Once completed you can call calculate wages with or without parameters.
         return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public int calculateWage() {
+        return calculateWage(0);
     }
 
     //Getters and setters for BaseSalary//
@@ -23,7 +43,7 @@ public class Employee1 {
 
     //Getters and Setters for hourlyRate//
     public void setHourlyRate(int hourlyRate) {
-        if (hourlyRate <= 0) {
+        if (hourlyRate < 0) {
             throw new IllegalArgumentException("Hourly rate must be more than 0");
         } else {
             this.hourlyRate = hourlyRate;
--- Practice/src/com/codewithcarrington/TextBoxMain.java
+++ Practice/src/com/codewithcarrington/TextBoxMain.java
@@ -16,11 +16,13 @@ public class TextBoxMain {
 //        System.out.println(textBox4.text);//Also prints out Hello World.
 
         //---Writing a program to calculate wage of employees--//
-        var employee = new Employee1();
-        employee.setBaseSalary(50_000); //Need this setter because BaseSalary is private in Employee1 class. In order to run program need to get and set.
-        employee.setHourlyRate(20);
-        int wage = employee.calculateWage(10); // This method is in Employee1 class
+        var employee2 = new Employee1(75_000);//Does not need second parameter due to overloading constructor in employee1 class.
+        var employee = new Employee1(50_000, 20); // We can delete code on line 21 and 22 since we set constructors
+//        employee.setBaseSalary(50_000); //Need this setter because BaseSalary is private in Employee1 class. In order to run program need to get and set.
+//        employee.setHourlyRate(20);
+        int wage = employee.calculateWage(); // This method is in Employee1 class
         System.out.println(wage);
+        Employee1.printNumberOfEmployees();
 
         //---Reducing Coupling---//
         var browser1 = new Browser1();
