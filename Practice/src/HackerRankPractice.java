a7fa12db6af859a12d4d1c87d8aa8da9e6a0b27b
Carrington Jones <carrington.jones47@gmail.com>
2021-08-05 23:14:31 -0500

    HackerRank Practice

--- Practice/src/HackerRankPractice.java
+++ Practice/src/HackerRankPractice.java
@@ -263,6 +263,39 @@ public class HackerRankPractice {
 //            System.out.println(newString);
 //        }
 
+//        Input format:
+//        There are two lines of input. The first line contains B : the breadth of the parallelogram. The next line contains E : the height of the parallelogram.
+
+//        Output format:
+//        If both values are greater than zero, then the main method must output the area of the parallelogram. Otherwise, print "java.lang.Exception: Breadth and height must be positive" without quotes.
+
+//        static Scanner input = new Scanner(System.in);
+//        static boolean flag = true;
+//        static int B = input.nextInt();
+//        static int H = input.nextInt();
+//
+//        static{
+//            try{
+//                if(B <= 0 || H <= 0){
+//                    flag = false;
+//                    throw new Exception("Breadth and height must be positive");
+//                }
+//            }catch(Exception e){
+//                System.out.println(e);
+//            }
+//        }
+//
+//        public static void main(String[] args){
+//            if(flag){
+//                int area=B*H;
+//                System.out.print(area);
+//            }
+//
+//        }//end of main
+//
+//    }//end of class
+
+
 
     }
 }
