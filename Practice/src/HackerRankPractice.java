7d611a077b05fc985fdcbe196b22630bbc4fb2cb
Carrington Jones <carrington.jones47@gmail.com>
2021-08-07 14:08:59 -0500

    HackerRank Practice

--- Practice/src/HackerRankPractice.java
+++ Practice/src/HackerRankPractice.java
@@ -296,6 +296,35 @@ public class HackerRankPractice {
 //    }//end of class
 
 
+//        Given a string A, print Yes if it is a palindrome, print No otherwise.
+//
+//        public class Solution {
+//
+//            static boolean isPalindrome (String A){
+//                int i = 0, j = A.length() - 1;
+//                while(i<j){
+//                    if(A.charAt(i) != A.charAt(j))
+//                        return false;
+//                    i++;
+//                    j--;
+//                }
+//                return true;
+//            }
+//
+//            public static void main(String[] args) {
+//
+//                Scanner sc=new Scanner(System.in);
+//                String A=sc.next();
+//
+//                if(isPalindrome(A))
+//                    System.out.println("Yes");
+//                else
+//                    System.out.println("No");
+//            }
+//        }
+
+
+
 
     }
 }
