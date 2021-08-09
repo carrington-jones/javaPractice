cf339b641c0922cf2101ff20aed9388ab729fba0
Carrington Jones <carrington.jones47@gmail.com>
2021-08-08 20:15:17 -0500

    HackerRank practice

--- Practice/src/HackerRankPractice.java
+++ Practice/src/HackerRankPractice.java
@@ -323,7 +323,27 @@ public class HackerRankPractice {
 //            }
 //        }
 
+//        Using Regex, we can easily match or search for patterns in a text. Before searching for a pattern, we have to specify one using some well-defined syntax.
+//        In this problem, you are given a pattern. You have to check whether the syntax of the given pattern is valid.
+//        Note: In this problem, a regex is only valid if you can compile it using the Pattern.compile method.
+//        For each test case, print Valid if the syntax of the given pattern is correct. Otherwise, print Invalid. Do not print the quotes.
 
+//        public class Solution {
+//            public static void main(String[] args){
+//                Scanner in = new Scanner(System.in);
+//                int testCases = Integer.parseInt(in.nextLine());
+//                while(testCases > 0){
+//                    String pattern = in.nextLine();
+//                    try {
+//                        Pattern.compile(pattern);
+//                        System.out.println("Valid");
+//                    } catch (PatternSyntaxException e) {
+//                        System.out.println("Invalid");
+//                    }
+//                    testCases--;
+//                }
+//            }
+//        }
 
 
     }
