876cf22510569cf90d0a55c428dab602147ae79d
Carrington Jones <carrington.jones47@gmail.com>
2021-08-18 22:16:56 -0500

    Java practice

---
+++ Practice/src/Bookstore/Bookstore.java
@@ -0,0 +1,10 @@
+package Bookstore;
+
+public class Bookstore {
+    public static void main(String[] args) {
+        Author JKRowling = new Author();
+        Author ErnestHemingway = new Author();
+        Author GeorgeOrwell = new Author();
+        System.out.println(JKRowling);
+    }
+}
