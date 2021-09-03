659c7a00094aef4b69cdd8fde24b3763b58f66c8
Carrington Jones <carrington.jones47@gmail.com>
2021-09-03 13:56:30 -0500

    Java Practice

---
+++ Practice/src/CodeupExtraExercises/JavaPracticeProblems.java
@@ -0,0 +1,26 @@
+package CodeupExtraExercises;
+
+public class JavaPracticeProblems {
+    public static void main(String[] args) {
+
+
+    }
+
+//    A palindrome is a word which reads the same backward or forward. 'abcba' is a palindrome.
+//    Write a method that detects if a string is a palindrome.
+
+//    SOLUTION:
+public boolean isPalindrome(String word) {
+    int i1 = 0;
+    int i2 = word.length() - 1;
+    while (i2 > i1) {
+        if (word.charAt(i1) != word.charAt(i2)) {
+            return false;
+        }
+        ++i1;
+        --i2;
+    }
+    return true;
+}
+
+}
