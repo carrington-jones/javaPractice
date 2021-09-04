254fcb27202974db500aff7862e2d74b7654a4f6
Carrington Jones <carrington.jones47@gmail.com>
2021-09-04 11:03:48 -0500

    Java practice problems. Missing element in an Array

--- Practice/src/CodeupExtraExercises/JavaPracticeProblems.java
+++ Practice/src/CodeupExtraExercises/JavaPracticeProblems.java
@@ -1,8 +1,15 @@
 package CodeupExtraExercises;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class JavaPracticeProblems {
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar"));
+//        System.out.println(isPalindrome("racecar"));
+
+        for (int n = 0; n < 10; ++n)
+            test(100000);
 
     }
 
@@ -10,17 +17,50 @@ public class JavaPracticeProblems {
 //    Write a method that detects if a string is a palindrome.
 
 //    SOLUTION:
-public static boolean isPalindrome(String word) {
-    int i1 = 0;
-    int i2 = word.length() - 1;
-    while (i2 > i1) {
-        if (word.charAt(i1) != word.charAt(i2)) {
-            return false;
+//public static boolean isPalindrome(String word) {
+//    int i1 = 0;
+//    int i2 = word.length() - 1;
+//    while (i2 > i1) {
+//        if (word.charAt(i1) != word.charAt(i2)) {
+//            return false;
+//        }
+//        ++i1;
+//        --i2;
+//    }
+//    return true;
+
+
+//    You are given an array of positive numbers from 1 to n, such that all numbers from 1 to n are present except one number ‘x’. You have to find ‘x’. The input array is not sorted.
+
+    static int find_missing(List<Integer> input) {
+        // calculate sum of all elements
+        // in input list
+        int sum_of_elements = 0;
+        for (Integer x : input) {
+            sum_of_elements += x;
         }
-        ++i1;
-        --i2;
+
+        // There is exactly 1 number missing
+        int n = input.size() + 1;
+        int actual_sum = (n * ( n + 1 ) ) / 2;
+        return actual_sum - sum_of_elements;
     }
-    return true;
-}
+    static void test(int n) {
+        int missing_element = (new Random()).nextInt(n) + 1;
+        List<Integer> v = new ArrayList<Integer>();
+        for(int i = 1; i <= n; ++i) {
+            if (i != missing_element)
+                v.add(i);
+        }
+
+        int actual_missing = find_missing(v);
+        System.out.print("Expected Missing = ");
+        System.out.print(missing_element);
+        System.out.print(" Actual Missing = ");
+        System.out.println(actual_missing);
+        System.out.println("Missing Element == Actual Missing : "+ (missing_element == actual_missing));
+    }
 
 }
+
+
